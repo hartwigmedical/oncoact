6b4abb806819282f7330b01996e91034df83d308
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2023-03-15 12:18:41 +0100

    Remove unused code

--- patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/cfreport/data/EvidenceItems.java
+++ patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/cfreport/data/EvidenceItems.java
@@ -1,15 +1,9 @@
 package com.hartwig.oncoact.patientreporter.cfreport.data;
 
-import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.StringJoiner;
 
-import com.google.common.collect.Sets;
 import com.hartwig.oncoact.patientreporter.cfreport.ReportResources;
-import com.hartwig.oncoact.protect.ProtectEvidence;
-import com.hartwig.serve.datamodel.EvidenceLevel;
 import com.itextpdf.kernel.pdf.action.PdfAction;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Text;
@@ -18,55 +12,10 @@ import org.jetbrains.annotations.NotNull;
 
 public final class EvidenceItems {
 
-    private static final String NONE = "None";
-
     private EvidenceItems() {
     }
 
-    public static int uniqueEventCount(@NotNull List<ProtectEvidence> evidenceItems) {
-        Set<String> events = Sets.newHashSet();
-        for (ProtectEvidence evidence : evidenceItems) {
-            String event = evidence.gene() != null ? evidence.gene() + " " + evidence.event() : evidence.event();
-            if (evidence.level().equals(EvidenceLevel.A) || evidence.level().equals(EvidenceLevel.B)) {
-                events.add(event);
-            }
-        }
-        return events.size();
-    }
-
-    @NotNull
-    public static String onLabelTreatmentString(@NotNull List<ProtectEvidence> protect) {
-        return treatmentString(protect, true, false);
-    }
-
-    @NotNull
-    private static String treatmentString(@NotNull List<ProtectEvidence> evidences, boolean requireOnLabel, boolean reportGermline) {
-        Set<EvidenceLevel> levels = Sets.newTreeSet(Comparator.naturalOrder());
-        Set<String> treatments = Sets.newHashSet();
-        for (ProtectEvidence evidence : evidences) {
-            if (evidence.onLabel() == requireOnLabel && (reportGermline || !evidence.germline())) {
-                if (evidence.level().equals(EvidenceLevel.A) || evidence.level().equals(EvidenceLevel.B)) {
-                    treatments.add(evidence.treatment().name());
-                    levels.add(evidence.level());
-                }
-            }
-        }
-
-        if (treatments.isEmpty()) {
-            return NONE;
-        } else {
-            StringJoiner joiner = new StringJoiner(", ");
-            for (EvidenceLevel level : levels) {
-                if (level.equals(EvidenceLevel.A) || level.equals(EvidenceLevel.B)) {
-                    joiner.add(level.toString());
-                }
-            }
-
-            return treatments.size() + " (" + joiner + ")";
-        }
-    }
-
-    @NotNull
+       @NotNull
     public static Paragraph createLinksPublications(@NotNull Set<String> evidenceUrls) {
         Paragraph paragraphPublications = new Paragraph();
         int number = 0;
