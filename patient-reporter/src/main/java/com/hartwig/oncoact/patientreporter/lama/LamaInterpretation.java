6168d6cfed9e279ca60fe52eac4bbe7a2e4d12c1
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2023-05-08 09:24:48 +0200

    OPR-33: Merge hospital data and add check for this

--- patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/lama/LamaInterpretation.java
+++ patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/lama/LamaInterpretation.java
@@ -1,12 +1,18 @@
 package com.hartwig.oncoact.patientreporter.lama;
 
+import com.hartwig.oncoact.patientreporter.PanelReporterApplication;
 import com.hartwig.oncoact.util.Formats;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.util.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.time.LocalDate;
 
 public class LamaInterpretation {
+
+    private static final Logger LOGGER = LogManager.getLogger(LamaInterpretation.class);
 
     private LamaInterpretation(){
     }
@@ -22,10 +28,43 @@ public class LamaInterpretation {
     }
 
     public static String hospitalContactReport(@Nullable String studyPI, @Nullable String requester, @Nullable String hospital,
-                                               @Nullable String postalCode, @Nullable String city, @Nullable String adres) {
-        //TODO; implement hospital contact data correctly
-        String mainRequester = requester;
-        String hospitalAdres = hospital + ", " + postalCode + ", " + city;
-        return mainRequester + ", " + hospitalAdres;
+                                               @Nullable String postalCode, @Nullable String city, @Nullable String address) {
+        String requesterNameReport = Strings.EMPTY;
+        if (studyPI!= null) {
+            requesterNameReport = studyPI;
+        } else if (requester != null) {
+            requesterNameReport = requester;
+        } else {
+            LOGGER.warn("None requester name of report is known. Solve before reporting!");
+        }
+
+        String hospitalString= Strings.EMPTY;
+        String postalCodeString= Strings.EMPTY;
+        String cityString= Strings.EMPTY;
+
+        if (hospital != null) {
+            hospitalString = hospital;
+        } else {
+            LOGGER.warn("Unknown hospital name is known");
+        }
+
+        if (postalCode != null) {
+            postalCodeString = postalCode;
+        } else {
+            LOGGER.warn("Unknown postal code of hospital known");
+        }
+
+        if (city != null) {
+            cityString = city;
+        } else {
+            LOGGER.warn("Unknown city of hospital known");
+        }
+
+        String hospitalAddress = hospitalString + ", " + postalCodeString + " " + cityString;
+        if (address != null) {
+            hospitalAddress = hospitalString + ", " + address + ", " + postalCodeString +" " + cityString;
+        }
+
+        return requesterNameReport + ", " + hospitalAddress;
     }
 }
--- patient-reporter/src/test/java/com/hartwig/oncoact/patientreporter/lama/LamaInterpretationTest.java
+++ patient-reporter/src/test/java/com/hartwig/oncoact/patientreporter/lama/LamaInterpretationTest.java
@@ -1,5 +1,6 @@
 package com.hartwig.oncoact.patientreporter.lama;
 
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -24,4 +25,55 @@ public class LamaInterpretationTest {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-LLL-yyyy");
         assertEquals(LamaInterpretation.extractEarliestArrivalDate(refDate, tumDate), tumDate.format(formatter));
     }
+
+    @Test
+    public void extractHospitalDataStudy(){
+
+        String studyPI = "studyPI";
+        String requester = null;
+        String hospital = "test center";
+        String postalCode = "123";
+        String city = "Amsterdam";
+        String address = "test 1";
+        String result = studyPI + ", " + hospital + ", " + address + ", " + postalCode + " " + city;
+
+        assertEquals(result, LamaInterpretation.hospitalContactReport(studyPI, requester, hospital, postalCode, city, address));
+    }
+
+    @Test
+    public void extractHospitalDataDiagnostic(){
+        String studyPI = null;
+        String requester = "requester";
+        String hospital = "test center";
+        String postalCode = "123";
+        String city = "Amsterdam";
+        String address = null;
+        String result = requester + ", " + hospital + ", " + postalCode + " " + city;
+        assertEquals(result, LamaInterpretation.hospitalContactReport(studyPI, requester, hospital, postalCode, city, address));
+    }
+
+    @Test
+    public void extractHospitalDataMissingNameOnReport(){
+        String studyPI = null;
+        String requester = null;
+        String hospital = "test center";
+        String postalCode = "123";
+        String city = "Amsterdam";
+        String address = null;
+        String result = ", " + hospital + ", " + postalCode + " " + city;
+        assertEquals(result, LamaInterpretation.hospitalContactReport(studyPI, requester, hospital, postalCode, city, address));
+    }
+
+    @Test
+    public void extractHospitalDataIncompleteHospitalDate(){
+        String studyPI = null;
+        String requester = "requester";
+        String hospital = null;
+        String postalCode = null;
+        String city = null;
+        String address = null;
+        String result = requester + ", " + ",  ";
+        assertEquals(result, LamaInterpretation.hospitalContactReport(studyPI, requester, hospital, postalCode, city, address));
+    }
+
 }
