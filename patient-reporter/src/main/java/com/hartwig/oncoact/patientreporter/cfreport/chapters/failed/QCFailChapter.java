3c5469ce9ef188b90b25234a6dbbdd3665c0ead6
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2023-06-27 16:37:32 +0200

    OPR-107: Use string from failed database for failed pdf generation

--- patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/cfreport/chapters/failed/QCFailChapter.java
+++ patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/cfreport/chapters/failed/QCFailChapter.java
@@ -55,60 +55,18 @@ public class QCFailChapter implements ReportChapter {
                 + " is based on information received from the originating hospital.").addStyle(ReportResources.subTextSmallStyle()));
         reportDocument.add(LineDivider.createLineDivider(contentWidth()));
 
-        reportDocument.add(createFailReasonDiv(failReport.reason()));
+        reportDocument.add(createFailReasonDiv(failReport.reportReason(), failReport.reportExplanation(), failReport.reportExplanationDetail()));
         reportDocument.add(LineDivider.createLineDivider(contentWidth()));
-
     }
 
     @NotNull
-    private static Div createFailReasonDiv(@NotNull QCFailReason failReason) {
-        String reason = Formats.NA_STRING;
-        String explanation = Formats.NA_STRING;
-        String explanationDetail = Formats.NA_STRING;
-
-        switch (failReason.type()) {
-            case LOW_QUALITY_BIOPSY: {
-                reason = "Insufficient quality of received biomaterial(s)";
-                explanation = "The received biomaterial(s) did not meet the requirements that are needed for \n"
-                        + "high quality Whole Genome Sequencing.";
-                break;
-            }
-            case TECHNICAL_FAILURE: {
-                reason = "Technical failure";
-                explanation = "Whole Genome Sequencing could not be successfully performed on the received biomaterial(s) \n"
-                        + "due to technical problems.";
-            }
-        }
-
-        switch (failReason) {
-            case INSUFFICIENT_DNA: {
-                explanationDetail =
-                        "Sequencing could not be performed due to insufficient DNA.";
-                break;
-            }
-            case INSUFFICIENT_TCP_DEEP_WGS:
-            case INSUFFICIENT_TCP_SHALLOW_WGS: {
-                explanationDetail = "The tumor percentage based on molecular estimation was below the minimal of 20% tumor cells \n"
-                        + "and could not be further analyzed.";
-                break;
-            }
-            case SUFFICIENT_TCP_QC_FAILURE: {
-                explanationDetail = "The tumor percentage based on molecular estimation was above the minimal of 20% tumor cells \n"
-                        + "but could not be further analyzed due to insufficient quality.";
-                break;
-            }
-            case TECHNICAL_FAILURE: {
-                explanationDetail = Strings.EMPTY;
-                break;
-            }
-        }
-
+    private static Div createFailReasonDiv(@NotNull String reportReason, @NotNull String reportExplanation, @NotNull String reportExplanationDetail) {
         Div div = new Div();
         div.setKeepTogether(true);
 
-        div.add(new Paragraph(reason).addStyle(ReportResources.dataHighlightStyle()));
-        div.add(new Paragraph(explanation).addStyle(ReportResources.bodyTextStyle()).setFixedLeading(ReportResources.BODY_TEXT_LEADING));
-        div.add(new Paragraph(explanationDetail).addStyle(ReportResources.subTextStyle())
+        div.add(new Paragraph(reportReason).addStyle(ReportResources.dataHighlightStyle()));
+        div.add(new Paragraph(reportExplanation).addStyle(ReportResources.bodyTextStyle()).setFixedLeading(ReportResources.BODY_TEXT_LEADING));
+        div.add(new Paragraph(reportExplanationDetail).addStyle(ReportResources.subTextStyle())
                 .setFixedLeading(ReportResources.BODY_TEXT_LEADING));
         return div;
     }
