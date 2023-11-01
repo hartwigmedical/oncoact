9000268cfc647b05e88efd39f34c90db431fe195
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2023-11-01 11:55:59 +0100

    INC-328; Revert back

--- patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/cfreport/components/SidePanel.java
+++ patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/cfreport/components/SidePanel.java
@@ -130,7 +130,7 @@ public final class SidePanel {
                 biopsySubLocation = biopsySite.getSubLocation();
                 biopsyLateralisation =
                         biopsySite.getLateralisation() != null ? biopsySite.getLateralisation().toString() : biopsyLateralisation;
-                isPrimaryTumor = biopsySite.getIsPrimaryTumor() != null ? (biopsySite.getIsPrimaryTumor() ? "Yes" : "No") : "-";
+                isPrimaryTumor = biopsySite.getIsPrimaryTumor() != null ? (biopsySite.getIsPrimaryTumor() ? "yes" : "no") : "-";
             }
 
             cv.add(createSidePanelDiv(++sideTextIndex, "Biopsy location", biopsyLocation));
