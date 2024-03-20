6fa3d3e8d8842764c0c8bfe47a2a1c24000db0f5
Mathijs den Burger <m.denburger@hartwigmedicalfoundation.nl>
2024-03-20 11:37:56 +0100

    OPR-157 Remove ObservedGainsLossesType immutable annotations

--- patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/model/ObservedGainsLossesType.java
+++ patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/model/ObservedGainsLossesType.java
@@ -1,11 +1,5 @@
 package com.hartwig.oncoact.patientreporter.model;
 
-import org.immutables.value.Value;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-@Value.Immutable
-@Value.Style(passAnnotations = {NotNull.class, Nullable.class})
 public enum ObservedGainsLossesType {
     FULL_GAIN,
     PARTIAL_GAIN,
