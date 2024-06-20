126ef0e07cca0a225e3ba6f62a77f574b2ed7aab
Lieke Schoenmaker <l.schoenmaker@hartwigmedicalfoundation.nl>
2024-06-20 10:24:44 +0200

    OPR-163: Solve issue negative reportable events for homozygous disruptions

--- patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/cfreport/chapters/analysed/EvidenceDirectionComparator.java
+++ patient-reporter/src/main/java/com/hartwig/oncoact/patientreporter/cfreport/chapters/analysed/EvidenceDirectionComparator.java
@@ -1,29 +1,21 @@
 package com.hartwig.oncoact.patientreporter.cfreport.chapters.analysed;
 
-import java.util.Comparator;
 import java.util.EnumMap;
 
 import com.hartwig.serve.datamodel.EvidenceDirection;
 
-public class EvidenceDirectionComparator implements Comparator<EvidenceDirection> {
+public class EvidenceDirectionComparator {
     private static final EnumMap<EvidenceDirection, Integer> evidenceDirectionOrder = new EnumMap<>(EvidenceDirection.class);
-    public static final EvidenceDirectionComparator INSTANCE = new EvidenceDirectionComparator();
 
-    static {
+    private EvidenceDirectionComparator() {
+    }
+
+    public static EnumMap<EvidenceDirection, Integer> generateDirectionPriorityMap() {
         evidenceDirectionOrder.put(EvidenceDirection.RESPONSIVE, 0);
         evidenceDirectionOrder.put(EvidenceDirection.PREDICTED_RESPONSIVE, 1);
         evidenceDirectionOrder.put(EvidenceDirection.RESISTANT, 2);
         evidenceDirectionOrder.put(EvidenceDirection.PREDICTED_RESISTANT, 3);
         evidenceDirectionOrder.put(EvidenceDirection.NO_BENEFIT, 4);
-    }
-
-    private EvidenceDirectionComparator() {
-    }
-
-    @Override
-    public int compare(EvidenceDirection evidenceDirection1, EvidenceDirection evidenceDirection2) {
-        Integer evidenceDirectionOrder1 = evidenceDirectionOrder.get(evidenceDirection1);
-        Integer evidenceDirectionOrder2 = evidenceDirectionOrder.get(evidenceDirection2);
-        return evidenceDirectionOrder1.compareTo(evidenceDirectionOrder2);
+        return evidenceDirectionOrder;
     }
 }
